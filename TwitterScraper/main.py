3698ca35e94c2e290b50ac20a9de28372de4b8d4
kartik-teja <kartikteja@gmail.com>
2024-09-05 10:51:38 +0530

    added a Web Scraper skeleton file

---
+++ TwitterScraper/main.py
@@ -0,0 +1,4 @@
+#The first step is the registration of your app. In particular, you need to point your browser to http://apps.twitter.com, log-in to Twitter (if you’re not already logged in) and register a new application.
+#Twitter provides REST APIs you can use to interact with their service. There is also a bunch of Python-based clients out there that we can use without re-inventing the wheel.
+#i.e Tweepy install it using pip
+
